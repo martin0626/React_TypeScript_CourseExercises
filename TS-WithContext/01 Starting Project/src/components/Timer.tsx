34cd72df36ba554b09951f147032d170eab27985
martin0626 <scbsteel12@gmail.com>
2025-01-26 13:14:10 +0200

    Added UseEffect for progress bar of the timers

--- TS-WithContext/01 Starting Project/src/components/Timer.tsx
+++ TS-WithContext/01 Starting Project/src/components/Timer.tsx
@@ -1,4 +1,5 @@
-import { Timer } from '../timers-ctx.tsx';
+import { useEffect, useRef, useState } from 'react';
+import { Timer, useTimersCtx } from '../timers-ctx.tsx';
 import Container from './UI/Container.tsx';
 
 
@@ -8,10 +9,41 @@ type TimerCompProps = {
 
 
 export default function TimerComp({timer}: TimerCompProps ) {
+  const [remainingTime, setRemTime] = useState(timer.duration * 1000);
+  const intervalRef = useRef<number | null>(null);
+  const { isRunning } = useTimersCtx();
+
+
+  if(remainingTime <= 0 && intervalRef.current){
+    clearInterval(intervalRef.current);
+  }
+
+
+  useEffect(()=>{
+    let timer: number;
+
+    if(isRunning){
+      timer = setInterval(()=> setRemTime(prevTiem=> {
+        if(prevTiem <= 0){
+          return prevTiem;
+        }
+        return prevTiem - 50
+      }), 50);
+      intervalRef.current = timer
+    }else if(intervalRef.current){
+      clearInterval(intervalRef.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  let formatedTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{timer.name}</h2>
-      <p>{timer.duration}</p>
+      <progress max={timer.duration} value={formatedTime} />
+      <p>{formatedTime}</p>
     </Container>
   );
 }
